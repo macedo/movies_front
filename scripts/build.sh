c10da52cab2e05b1fca9784ff0baef3b7679ba46
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2016-06-14 22:28:59 -0700

    deploy prod heroku

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,11 @@
+if [ -z "$ENV" ]; then
+  ENV="development"
+fi
+
+#JS
 babel --presets react,es2015 js/source -d js/build
-browserify js/build/app.js -o bundle.js
-cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > bundle.css
+browserify js/build/app.js -o public/js/bundle.js
+
+#CSS
+cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > public/css/bundle.css
 date; echo;
